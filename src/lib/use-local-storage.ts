69f760f9fc97f775834c7c3ae3b182f900510fb4
jakubfiglak <jakub.figlak@netguru.pl>
2022-07-07 22:38:15 +0200

    fix: useLocalStorage hook for ssr

--- src/lib/use-local-storage.ts
+++ src/lib/use-local-storage.ts
@@ -9,9 +9,11 @@ export function useLocalStorage<ValueType>({
   key,
   initialValue,
 }: IUseLocalStorageArgs<ValueType>) {
+  const isBrowser = typeof window !== 'undefined'
+
   const [storedValue, setStoredValue] = useState<ValueType>(() => {
     try {
-      const localStorageValue = localStorage.getItem(key)
+      const localStorageValue = isBrowser ? localStorage.getItem(key) : null
       return localStorageValue ? JSON.parse(localStorageValue) : initialValue
     } catch (error) {
       console.error(error)
@@ -24,7 +26,9 @@ export function useLocalStorage<ValueType>({
       const valueToStore =
         value instanceof Function ? value(storedValue) : value
       setStoredValue(valueToStore)
-      localStorage.setItem(key, JSON.stringify(valueToStore))
+      if (isBrowser) {
+        localStorage.setItem(key, JSON.stringify(valueToStore))
+      }
     } catch (error) {
       console.error(error)
     }
