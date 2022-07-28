6ba9ab2fdbef9f0036803d8d1ae4b8fba0296615
jakubfiglak <jakub.figlak@netguru.pl>
2022-07-28 20:10:14 +0200

    refactor: move clubs api methods to service folder

--- src/services/api/methods/clubs.ts
+++ src/services/api/methods/clubs.ts
@@ -1,6 +1,37 @@
-import { ClubDto } from '@/modules/clubs/types'
+import {
+  ClubBasicDataDto,
+  ClubDto,
+  CreateClubDto,
+  FindAllClubsParams,
+  UpdateClubDto,
+} from '@/modules/clubs/types'
 
-import { getAssetBySlug } from './helpers'
+import {
+  createDocument,
+  deleteDocument,
+  getAssetBySlug,
+  getDataList,
+  getPaginatedData,
+  updateDocument,
+} from './helpers'
 
 export const getClubBySlug = (slug: string, token?: string) =>
   getAssetBySlug<ClubDto>({ moduleName: 'clubs', slug, token })
+
+export const getClubsList = () => getDataList<ClubBasicDataDto>('clubs')
+
+export const getClubs = (params: FindAllClubsParams) =>
+  getPaginatedData<FindAllClubsParams, ClubDto>(params, 'clubs')
+
+export const createClub = (data: CreateClubDto) =>
+  createDocument<CreateClubDto, ClubDto>(data, 'clubs')
+
+interface IUpdateClubArgs {
+  id: number
+  data: UpdateClubDto
+}
+
+export const updateClub = ({ id, data }: IUpdateClubArgs) =>
+  updateDocument<UpdateClubDto, ClubDto>(id, data, 'clubs')
+
+export const deleteClub = (id: number) => deleteDocument<ClubDto>(id, 'clubs')
