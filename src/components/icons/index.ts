ad137c5a80b263ea5d0f67be52b3d80a36f19efd
jakubfiglak <jakub.figlak@netguru.pl>
2022-07-31 14:04:32 +0200

    feat: create single match page

---
+++ src/components/icons/index.ts
@@ -0,0 +1,17 @@
+export {
+  PermContactCalendar as AdminIcon,
+  Security as ClubsIcon,
+  Storage as DatabaseIcon,
+  Home as HomeIcon,
+  Sports as MatchesIcon,
+  Note as NotesIcon,
+  Visibility as ObservationIcon,
+  Assignment as OrdersIcon,
+  DirectionsRun as PlayersIcon,
+  AccountCircle as ProfileIcon,
+  Assessment as ReportsIcon,
+  Create as ReportTemplatesIcon,
+  Settings as SettingsIcon,
+  GroupWork as TeamsIcon,
+  Person as UserDataIcon,
+} from '@mui/icons-material'
