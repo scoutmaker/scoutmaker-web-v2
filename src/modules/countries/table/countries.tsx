85263449e1e8d94ee621872962b9cdcc13a3a94e
dejwi <misti.krasula@gmail.com>
2022-08-02 15:43:35 +0200

    added table

---
+++ src/modules/countries/table/countries.tsx
@@ -0,0 +1,49 @@
+import { TFunction, useTranslation } from 'next-i18next'
+import { ReactNode } from 'react'
+
+import { Table } from '@/components/tables/table'
+import { ICommonTableProps, IHeadCell } from '@/types/tables'
+
+interface ITeamsTableProps extends ICommonTableProps {
+  children: ReactNode
+}
+
+function generateHeadCells(t: TFunction): IHeadCell[] {
+  return [
+    { id: 'name', label: t('NAME') },
+    { id: 'code', label: 'Code' /* t('') NO-TRANSLATION */ },
+    { id: 'isEuMember', label: 'IsEuMember' /* t('') NO-TRANSLATION */},
+  ]
+}
+
+export const CountriesTable = ({
+  page,
+  rowsPerPage,
+  sortBy,
+  order,
+  handleChangePage,
+  handleChangeRowsPerPage,
+  handleSort,
+  total,
+  actions,
+  children,
+}: ITeamsTableProps) => {
+  const { t } = useTranslation()
+
+  return (
+    <Table
+      page={page}
+      rowsPerPage={rowsPerPage}
+      sortBy={sortBy}
+      order={order}
+      handleChangePage={handleChangePage}
+      handleChangeRowsPerPage={handleChangeRowsPerPage}
+      handleSort={handleSort}
+      total={total}
+      headCells={generateHeadCells(t)}
+      actions={actions}
+    >
+      {children}
+    </Table>
+  )
+}
