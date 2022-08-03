0fb6a65cedee78360ac4ed1147b1e8b664f70624
dejwi <misti.krasula@gmail.com>
2022-08-03 15:59:56 +0200

    feat: add regions types

--- src/modules/regions/types.ts
+++ src/modules/regions/types.ts
@@ -1,1 +1,4 @@
 export type RegionDto = Components.Schemas.RegionDto
+export type CreateRegionDto = Components.Schemas.CreateRegionDto
+export type UpdateRegionDto = Components.Schemas.UpdateRegionDto
+export type RegionsFilterDto = Pick<Paths.RegionsControllerFindAll.QueryParameters, 'name' | 'countryId'>
