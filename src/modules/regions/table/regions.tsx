17a82fdf229ccb01886c69f8069c19d7921942f8
dejwi <misti.krasula@gmail.com>
2022-08-03 15:08:03 +0200

    feat: add regions table comps

---
+++ src/modules/regions/table/regions.tsx
@@ -0,0 +1,54 @@
+import { TFunction, useTranslation } from 'next-i18next'
+import { ReactNode } from 'react'
+
+import { Table } from '@/components/tables/table'
+import { ICommonTableProps, IHeadCell } from '@/types/tables'
+
+interface IRegionsTableProps extends ICommonTableProps {
+  children: ReactNode
+}
+
+function generateHeadCells(t: TFunction): IHeadCell[] {
+  return [
+    { id: 'name', label: t('NAME') },
+    { id: 'countryId', label: t('COUNTRY') },
+    { id: 'countryCode', label: t('countries:CODE'), isSortingDisabled: true },
+    {
+      id: 'isEuMember',
+      label: t('countries:IS_EU_MEMBER'),
+      isSortingDisabled: true,
+    },
+  ]
+}
+
+export const RegionsTable = ({
+  page,
+  rowsPerPage,
+  sortBy,
+  order,
+  handleChangePage,
+  handleChangeRowsPerPage,
+  handleSort,
+  total,
+  actions,
+  children,
+}: IRegionsTableProps) => {
+  const { t } = useTranslation()
+
+  return (
+    <Table
+      page={page}
+      rowsPerPage={rowsPerPage}
+      sortBy={sortBy}
+      order={order}
+      handleChangePage={handleChangePage}
+      handleChangeRowsPerPage={handleChangeRowsPerPage}
+      handleSort={handleSort}
+      total={total}
+      headCells={generateHeadCells(t)}
+      actions={actions}
+    >
+      {children}
+    </Table>
+  )
+}
