4ece4326e2771836fdd2cd4659bc6ae945ca2448
dejwi <misti.krasula@gmail.com>
2022-08-03 16:38:54 +0200

    feat: add regions forms

---
+++ src/modules/regions/forms/create.tsx
@@ -0,0 +1,74 @@
+import { TextField } from '@mui/material'
+import { Field, Form, Formik } from 'formik'
+import filter from 'just-filter-object'
+import { useTranslation } from 'next-i18next'
+import { Container } from '@/components/forms/container'
+import { MainFormActions } from '@/components/forms/main-form-actions'
+import { useAlertsState } from '@/context/alerts/useAlertsState'
+import { CountriesCombo } from '@/modules/countries/combo'
+import { generateCreateRegionValidationSchema, initialValues } from './utils'
+import { CreateRegionDto } from '../types'
+import { CountryDto } from '@/modules/countries/types'
+
+interface ICreateRegionFormProps {
+  countriesData: CountryDto[]
+  onSubmit: (data: CreateRegionDto) => void
+  onCancelClick?: () => void
+  fullwidth?: boolean
+}
+
+export const CreateRegionForm = ({
+  onSubmit,
+  onCancelClick,
+  fullwidth,
+  countriesData
+}: ICreateRegionFormProps) => {
+  const { setAlert } = useAlertsState()
+  const { t } = useTranslation()
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={generateCreateRegionValidationSchema(t)}
+      enableReinitialize
+      onSubmit={(data, { resetForm }) => {
+        const dataToSubmit = filter(data, (_, value) => value)
+        onSubmit(dataToSubmit as CreateRegionDto)
+        resetForm()
+      }}
+    >
+      {({ handleReset, touched, errors }) => (
+        <Form>
+          <Container fullwidth={fullwidth}>
+            <Field
+              name="name"
+              as={TextField}
+              variant="outlined"
+              fullWidth
+              label={t('NAME')}
+              error={touched.name && !!errors.name}
+              helperText={touched.name && errors.name}
+            />
+            <CountriesCombo
+              data={countriesData}
+              name="countryId"
+              label={t('COUNTRY')}
+              error={touched.countryId && !!errors.countryId}
+              helperText={touched.countryId ? errors.countryId : undefined}
+            />
+            <MainFormActions
+              label={t('COUNTRY')}
+              onCancelClick={() => {
+                if (onCancelClick) {
+                  onCancelClick()
+                }
+                handleReset()
+                setAlert({ msg: t('CHANGES_CANCELLED'), type: 'warning' })
+              }}
+            />
+          </Container>
+        </Form>
+      )}
+    </Formik>
+  )
+}
