cb03fe261a3411242749126bd0596b13760b8fb9
dejwi <misti.krasula@gmail.com>
2022-08-05 18:48:32 +0200

    feat: add countries methods

--- src/modules/countries/hooks.ts
+++ src/modules/countries/hooks.ts
@@ -1,9 +1,20 @@
-import { CountryDto } from '@/modules/countries/types'
-import { getCountriesList } from '@/services/api/methods/countries'
+import { CountryDto, FindAllCountriesParams } from '@/modules/countries/types'
+import {
+  getCountries,
+  getCountriesList,
+} from '@/services/api/methods/countries'
 import { TModuleName } from '@/services/api/modules'
 import { useList } from '@/utils/hooks/api/use-list'
+import { usePaginatedData } from '@/utils/hooks/api/use-paginated-data'
 
 const moduleName: TModuleName = 'countries'
 
 export const useCountriesList = () =>
   useList<CountryDto>(moduleName, getCountriesList)
+
+export const useCountries = (params: FindAllCountriesParams) =>
+  usePaginatedData<FindAllCountriesParams, CountryDto>(
+    moduleName,
+    params,
+    getCountries,
+  )
--- src/services/api/methods/countries.ts
+++ src/services/api/methods/countries.ts
@@ -1,8 +1,11 @@
-import { CountryDto } from '@/modules/countries/types'
+import { CountryDto, FindAllCountriesParams } from '@/modules/countries/types'
 import { TModuleName } from '@/services/api/modules'
 
-import { getDataList } from './helpers'
+import { getDataList, getPaginatedData } from './helpers'
 
 const moduleName: TModuleName = 'countries'
 
 export const getCountriesList = () => getDataList<CountryDto>(moduleName)
+
+export const getCountries = (params: FindAllCountriesParams) =>
+  getPaginatedData<FindAllCountriesParams, CountryDto>(params, moduleName)
