de201f142b0d2539dee7eb5d50b60fcbc756d6e5
dejwi <misti.krasula@gmail.com>
2022-08-05 18:46:49 +0200

    feat: add types

--- src/modules/countries/types.ts
+++ src/modules/countries/types.ts
@@ -1,6 +1,16 @@
 export type CountryDto = Components.Schemas.CountryDto
 
-export type CountriesFiltersDto = Pick<Paths.CountriesControllerFindAll.QueryParameters, 'isEuMember'>
+export type CountriesFiltersDto = Pick<
+  Paths.CountriesControllerFindAll.QueryParameters,
+  'isEuMember'
+>
 
 export type CreateCountryDto = Components.Schemas.CreateCountryDto
 export type UpdateCountryDto = Components.Schemas.UpdateCountryDto
+
+export type CountriesSortBy = Paths.CountriesControllerFindAll.Parameters.SortBy
+
+export type FindAllCountriesParams = Pick<
+  Paths.CountriesControllerFindAll.QueryParameters,
+  'isEuMember' | 'limit' | 'page' | 'sortBy' | 'sortingOrder'
+>
