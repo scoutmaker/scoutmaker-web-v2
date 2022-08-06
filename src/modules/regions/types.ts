f9ec9fe00d372a0a81dc40e5c78fc58abbe1be66
dejwi <misti.krasula@gmail.com>
2022-08-06 11:43:37 +0200

    feat: formatted

--- src/modules/regions/types.ts
+++ src/modules/regions/types.ts
@@ -1,4 +1,7 @@
 export type RegionDto = Components.Schemas.RegionDto
 export type CreateRegionDto = Components.Schemas.CreateRegionDto
 export type UpdateRegionDto = Components.Schemas.UpdateRegionDto
-export type RegionsFilterDto = Pick<Paths.RegionsControllerFindAll.QueryParameters, 'name' | 'countryId'>
+export type RegionsFilterDto = Pick<
+  Paths.RegionsControllerFindAll.QueryParameters,
+  'name' | 'countryId'
+>
