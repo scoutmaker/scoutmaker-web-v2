d811810527e756af9172bea3da8ed763d2a88261
dejwi <misti.krasula@gmail.com>
2022-08-06 12:35:06 +0200

    feat: add create regions page

---
+++ src/pages/regions/create.tsx
@@ -0,0 +1,35 @@
+import { useTranslation } from 'next-i18next'
+
+import { Loader } from '@/components/loader/loader'
+import { PageHeading } from '@/components/page-heading/page-heading'
+import { useCountriesList } from '@/modules/countries/hooks'
+import { CreateRegionForm } from '@/modules/regions/forms/create'
+import { useCreateRegion } from '@/modules/regions/hooks'
+import { withSessionSsrRole } from '@/utils/withSessionSsrRole'
+
+export const getServerSideProps = withSessionSsrRole(['common', 'regions'], ['ADMIN'])
+
+const CreateTeamPage = () => {
+  const { t } = useTranslation()
+
+  const { data: countries, isLoading: countriesLoading } = useCountriesList()
+
+  const { mutate: createRegion, isLoading: createRegionLoading } = useCreateRegion()
+
+  const isLoading =
+    createRegionLoading ||
+    countriesLoading
+
+  return (
+    <>
+      {isLoading && <Loader />}
+      <PageHeading title={t('regions:CREATE_PAGE_TITLE')} />
+      <CreateRegionForm
+        countriesData={countries || []}
+        onSubmit={createRegion}
+      />
+    </>
+  )
+}
+
+export default CreateTeamPage
