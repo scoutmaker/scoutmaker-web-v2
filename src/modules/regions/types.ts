6cf93a974eeabda3c8ad73f9d616a46e793a6f8a
dejwi <misti.krasula@gmail.com>
2022-08-06 12:23:56 +0200

    feat: add types

--- src/modules/regions/types.ts
+++ src/modules/regions/types.ts
@@ -5,3 +5,8 @@ export type RegionsFilterDto = Pick<
   Paths.RegionsControllerFindAll.QueryParameters,
   'name' | 'countryId'
 >
+export type FindAllRegionsParams = Pick<
+  Paths.RegionsControllerFindAll.QueryParameters,
+  'countryId' | 'limit' | 'name' | 'page' | 'sortBy' | 'sortingOrder'
+>
+export type RegionsSortBy = Paths.RegionsControllerFindAll.Parameters.SortBy
