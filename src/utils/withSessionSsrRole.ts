935bb484459b79f2297dc713353777fa9357c252
dejwi <misti.krasula@gmail.com>
2022-08-08 10:57:10 +0200

    feat: pass data support

--- src/utils/withSessionSsrRole.ts
+++ src/utils/withSessionSsrRole.ts
@@ -1,20 +1,27 @@
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+import { ParsedUrlQuery } from 'node:querystring'
 
 import { withSessionSsr } from '@/modules/auth/session'
+import { ApiError } from '@/services/api/types'
 
 import { redirectToLogin } from './redirect-to-login'
 
-export type ISsrRole = {
+export type ISsrRole<T = null> = {
   errorStatus: number | null
   errorMessage: string | null
+  data: T | null
 }
 type TRole = Components.Schemas.UserDto['role']
 
-export const withSessionSsrRole = (
+export function withSessionSsrRole<T>(
   _translations: string[],
   requiredRoles: TRole[],
-) =>
-  withSessionSsr<ISsrRole>(async ({ locale, req, res }) => {
+  getData?: (
+    token: string,
+    params: ParsedUrlQuery,
+  ) => Promise<{ data: T | null; error?: ApiError }>,
+) {
+  return withSessionSsr<ISsrRole<T>>(async ({ locale, req, res, params }) => {
     const { user } = req.session
 
     if (!user) {
@@ -23,6 +30,7 @@ export const withSessionSsrRole = (
         props: {
           errorStatus: null,
           errorMessage: null,
+          data: null,
         },
       }
     }
@@ -38,6 +46,34 @@ export const withSessionSsrRole = (
           ...translations,
           errorStatus: 401,
           errorMessage: 'Insufficient Permissions',
+          data: null,
+        },
+      }
+    }
+
+    if (getData) {
+      const resd = await getData(
+        req.session.token as string,
+        params as ParsedUrlQuery,
+      )
+      if (resd.error) {
+        const { response } = resd.error
+
+        return {
+          props: {
+            ...translations,
+            errorStatus: response.status,
+            errorMessage: response.data.message,
+            data: null,
+          },
+        }
+      }
+      return {
+        props: {
+          ...translations,
+          errorStatus: null,
+          errorMessage: null,
+          data: resd.data,
         },
       }
     }
@@ -47,6 +83,8 @@ export const withSessionSsrRole = (
         ...translations,
         errorStatus: null,
         errorMessage: null,
+        data: null,
       },
     }
   })
+}
