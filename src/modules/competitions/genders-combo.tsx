64b2bee1e3f101a8aa492294b8a5cda3d6bbcb9b
dejwi <misti.krasula@gmail.com>
2022-08-10 12:59:20 +0200

    feat: add helper combo

---
+++ src/modules/competitions/genders-combo.tsx
@@ -0,0 +1,42 @@
+import { AutocompleteRenderInputParams, TextField } from '@mui/material'
+import { Field } from 'formik'
+import { Autocomplete } from 'formik-mui'
+import { useTranslation } from 'next-i18next'
+
+import { IComboProps } from '../../types/combo'
+
+export const GendersCombo = ({
+  name,
+  label,
+  multiple,
+  error,
+  helperText,
+  size,
+}: Omit<IComboProps<{}>, 'data'>) => {
+  const { t } = useTranslation()
+
+  return (
+    <Field
+      name={name}
+      component={Autocomplete}
+      multiple={multiple}
+      id={name}
+      size={size}
+      options={['MALE', 'FEMALE']}
+      getOptionLabel={(option: string) => {
+        if (option === 'MALE') return t('MALE')// ADD_TRANS
+        return t('FEMALE')// ADD_TRANS
+      }}
+      filterSelectedOptions
+      renderInput={(params: AutocompleteRenderInputParams) => (
+        <TextField
+          {...params}
+          error={error}
+          helperText={helperText}
+          label={label || t('GENDERS')}
+          placeholder={label || t('GENDERS')} // ADD_TRANS
+        />
+      )}
+    />
+  )
+}
