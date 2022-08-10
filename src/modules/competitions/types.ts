81795597f5297fa5aa2405145ad9bda382dea6f2
dejwi <misti.krasula@gmail.com>
2022-08-10 12:59:14 +0200

    feat: add types

--- src/modules/competitions/types.ts
+++ src/modules/competitions/types.ts
@@ -1,3 +1,9 @@
 export type CompetitionBasicDataDto = Components.Schemas.CompetitionBasicDataDto
 export type CompetitionParticipationWithoutTeamDto =
   Components.Schemas.CompetitionParticipationWithoutTeamDto
+
+export type CompetitionDto = Components.Schemas.CompetitionDto
+
+export type CreateCompetitonDto = Components.Schemas.CreateCompetitionDto
+
+export type UpdateCompetitionDto = Components.Schemas.UpdateCompetitionDto
