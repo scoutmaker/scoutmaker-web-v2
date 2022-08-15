ec05428b658c055eb52831ce9b7a37ad49f505eb
dejwi <misti.krasula@gmail.com>
2022-08-15 13:34:39 +0200

    feat: renamed type

--- src/modules/competitions/details-card.tsx
+++ src/modules/competitions/details-card.tsx
@@ -11,11 +11,11 @@ import { CardItemBasic } from '@/components/details-card-item'
 import { CompetitionIcon } from '@/components/icons'
 import { CompetitionDto } from '@/modules/competitions/types'
 
-type TDetailsCard = {
+type TCompetitionDetailsCard = {
   comp: CompetitionDto
 }
 
-export const CompetitionDetailsCard = ({ comp }: TDetailsCard) => {
+export const CompetitionDetailsCard = ({ comp }: TCompetitionDetailsCard) => {
   const { t } = useTranslation()
 
   const { ageCategory, country, gender, level, name, type, juniorLevel } = comp
