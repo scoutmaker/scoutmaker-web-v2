c31242b0385ac0c62d788fbaaf0e42cb41e8a260
dejwi <misti.krasula@gmail.com>
2022-08-16 12:42:25 +0200

    feat: changed from combo to select

---
+++ src/modules/orders/status-select.tsx
@@ -0,0 +1,32 @@
+import { MenuItem } from '@mui/material'
+import { Field } from 'formik'
+import { Select } from 'formik-mui'
+import { useTranslation } from 'next-i18next'
+
+import { IComboProps } from '../../types/combo'
+
+export const StatusSelect = ({
+  name,
+  label,
+  error,
+  helperText,
+  size,
+}: Omit<IComboProps<{}>, 'data'>) => {
+  const { t } = useTranslation()
+
+  return (
+    <Field
+      name={name}
+      component={Select}
+      label={label}
+      id={name}
+      size={size}
+      error={error}
+      helperText={helperText}
+    >
+      <MenuItem value="OPEN">{t('OPEN')}</MenuItem>
+      <MenuItem value="ACCEPTED">{t('ACCEPTED')}</MenuItem>
+      <MenuItem value="CLOSED">{t('CLOSED')}</MenuItem>
+    </Field>
+  )
+}
