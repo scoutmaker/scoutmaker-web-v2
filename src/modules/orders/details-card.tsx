435fb5d0f854a6bf34a5fc5a1675ae294d94a286
dejwi <misti.krasula@gmail.com>
2022-08-17 11:17:22 +0200

    feat: add order actions

--- src/modules/orders/details-card.tsx
+++ src/modules/orders/details-card.tsx
@@ -1,18 +1,26 @@
-import { Avatar, Card, CardContent, CardHeader, Grid } from '@mui/material'
+import { Avatar, Card, CardContent, CardHeader, Grid, IconButton, Tooltip } from '@mui/material'
 import { useTranslation } from 'next-i18next'
 
 import { CardItemBasic } from '@/components/details-card/details-card-item'
-import { OrdersIcon } from '@/components/icons'
+import { AcceptIcon, CloseIcon, OrdersIcon, RejectIcon } from '@/components/icons'
+import { Loader } from '@/components/loader/loader'
 import { formatDate } from '@/utils/format-date'
 
+import { useAcceptOrder, useCloseOrder, useRejectOrder } from './hooks'
 import { OrderDto } from './types'
 
 export const OrderDetailsCard = ({ order }: IDetailsCard) => {
   const { t } = useTranslation()
 
-  const { status, player, createdAt, description, match, scout } = order
+  const { status, player, createdAt, description, match, scout, id } = order
 
-  return (
+  const { mutate: acceptOrder, isLoading: acceptLoading } = useAcceptOrder()
+  const { mutate: rejectOrder, isLoading: rejectLoading } = useRejectOrder()
+  const { mutate: closeOrder, isLoading: closeLoading } = useCloseOrder()
+
+  const isLoading = acceptLoading || rejectLoading || closeLoading
+  return (<>
+    {isLoading && <Loader />}
     <Card sx={{ maxWidth: 700, margin: '0 auto' }}>
       <CardHeader
         avatar={
@@ -52,10 +60,40 @@ export const OrderDetailsCard = ({ order }: IDetailsCard) => {
             title={t('SCOUT')} // ADD_TRANS
             value={scout ? `${scout.firstName} ${scout.lastName}` : ''}
           />
+          <Grid container justifyContent='center'>
+            {status === 'OPEN' ?
+              <Tooltip title={t('orders:ACCEPT')}>
+                <IconButton
+                  aria-label="accept order"
+                  onClick={() => acceptOrder(id)}
+                >
+                  <AcceptIcon />
+                </IconButton>
+              </Tooltip> :
+              <>
+                <Tooltip title={t('CLOSE')}>
+                  <IconButton
+                    aria-label="close order"
+                    onClick={() => closeOrder(id)}
+                  >
+                    <CloseIcon />
+                  </IconButton>
+                </Tooltip>
+
+                <Tooltip title={t('REJECT')}>
+                  <IconButton
+                    aria-label="reject order"
+                    onClick={() => rejectOrder(id)}
+                  >
+                    <RejectIcon />
+                  </IconButton>
+                </Tooltip>
+              </>}
+          </Grid>
         </Grid>
       </CardContent>
     </Card>
-  )
+  </>)
 }
 
 interface IDetailsCard {
