320857da7ea525853ab4ee6a018c25eda35614ff
dejwi <misti.krasula@gmail.com>
2022-08-18 18:58:08 +0200

    feat: pr changes

--- src/modules/orders/status-select.tsx
+++ src/modules/orders/status-select.tsx
@@ -1,6 +1,7 @@
 import { MenuItem } from '@mui/material'
 import { Field } from 'formik'
 import { Select } from 'formik-mui'
+import { useTranslation } from 'next-i18next'
 
 import { IComboProps } from '../../types/combo'
 import { OrderStatusChip } from './StatusChip'
@@ -11,19 +12,23 @@ export const StatusSelect = ({
   error,
   helperText,
   size,
-}: Omit<IComboProps<{}>, 'data'>) => (
-  <Field
-    name={name}
-    component={Select}
-    label={label}
-    id={name}
-    size={size}
-    error={error}
-    helperText={helperText}
-  >
-    <MenuItem value="" />
-    <MenuItem value="OPEN"><OrderStatusChip status='OPEN' /></MenuItem>
-    <MenuItem value="ACCEPTED"><OrderStatusChip status='ACCEPTED' /></MenuItem>
-    <MenuItem value="CLOSED"><OrderStatusChip status='CLOSED' /></MenuItem>
-  </Field>
-)
+}: Omit<IComboProps<{}>, 'data'>) => {
+  const { t } = useTranslation()
+
+  return (
+    <Field
+      name={name}
+      component={Select}
+      label={label}
+      id={name}
+      size={size}
+      error={error}
+      helperText={helperText}
+    >
+      <MenuItem value="" >{t('ALL')}</MenuItem>
+      <MenuItem value="OPEN">{t('orders:OPEN')}</MenuItem>
+      <MenuItem value="ACCEPTED">{t('orders:ACCEPTED')}</MenuItem>
+      <MenuItem value="CLOSED">{t('orders:CLOSED')}</MenuItem>
+    </Field>
+  )
+}
