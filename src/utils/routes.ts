9003ba39c525f91cd7085d94bbf800095516e461
jakubfiglak <jakub.figlak@netguru.pl>
2022-08-19 17:07:36 +0200

    feat: add translations

--- src/utils/routes.ts
+++ src/utils/routes.ts
@@ -1,14 +1,14 @@
 export enum Routes {
-  PLAYERS = 'players',
-  TEAMS = 'teams',
-  MATCHES = 'matches',
-  REPORT_SKILL_ASSESSMENT_CATEGORIES = 'report-skill-assessment-categories',
+  PLAYERS = '/players',
+  TEAMS = '/teams',
+  MATCHES = '/matches',
+  REPORT_SKILL_ASSESSMENT_CATEGORIES = '/report-skill-assessment-categories',
 }
 
 export function getCreateRoute(route: Routes) {
-  return `/${route}/create`
+  return `${route}/create`
 }
 
 export function getEditRoute(route: Routes, identifier: string | number) {
-  return `/${route}/edit/${identifier}`
+  return `${route}/edit/${identifier}`
 }
