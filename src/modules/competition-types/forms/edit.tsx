4881a6a7766d0bc91fe000d9e73f26b90af46937
dejwi <misti.krasula@gmail.com>
2022-08-23 13:26:56 +0200

    feat: add competition types module

---
+++ src/modules/competition-types/forms/edit.tsx
@@ -0,0 +1,76 @@
+import { TextField } from '@mui/material'
+import { updatedDiff } from 'deep-object-diff'
+import { Field, Form, Formik } from 'formik'
+import filter from 'just-filter-object'
+import { useTranslation } from 'next-i18next'
+
+import { Container } from '@/components/forms/container'
+import { MainFormActions } from '@/components/forms/main-form-actions'
+import { useAlertsState } from '@/context/alerts/useAlertsState'
+
+import { CompetitionTypeDto, UpdateCompetitionTypeDto } from '../types'
+import {
+  generateUpdateValidationSchema,
+  getInitialStateFromCurrent,
+} from './utils'
+
+interface IEditRegionFormProps {
+  current: CompetitionTypeDto
+  onSubmit: (data: UpdateCompetitionTypeDto) => void
+  onCancelClick?: () => void
+  fullwidth?: boolean
+}
+
+export const EditCompetitionTypeForm = ({
+  current,
+  onSubmit,
+  onCancelClick,
+  fullwidth
+}: IEditRegionFormProps) => {
+  const { setAlert } = useAlertsState()
+  const { t } = useTranslation()
+
+  const initialValues = getInitialStateFromCurrent(current)
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={() => generateUpdateValidationSchema()}
+      enableReinitialize
+      onSubmit={data => {
+        const dataToSubmit = updatedDiff(
+          initialValues,
+          filter(data, (_, value) => value),
+        )
+        onSubmit(dataToSubmit)
+      }}
+    >
+      {({ handleReset, touched, errors }) => (
+        <Form>
+          <Container fullwidth={fullwidth}>
+            <Field
+              name="name"
+              as={TextField}
+              variant="outlined"
+              fullWidth
+              label={t('NAME')}
+              error={touched.name && !!errors.name}
+              helperText={touched.name && errors.name}
+            />
+            <MainFormActions
+              label={t('COMPETITION_TYPE')}
+              isEditState
+              onCancelClick={() => {
+                if (onCancelClick) {
+                  onCancelClick()
+                }
+                handleReset()
+                setAlert({ msg: t('CHANGES_CANCELLED'), type: 'warning' })
+              }}
+            />
+          </Container>
+        </Form>
+      )}
+    </Formik>
+  )
+}
