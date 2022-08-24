688a87b9842433dde805a55af69a653064c990d4
dejwi <misti.krasula@gmail.com>
2022-08-24 14:36:35 +0200

    feat: add competition junior levels module

--- src/modules/competition-junior-levels/types.ts
+++ src/modules/competition-junior-levels/types.ts
@@ -5,3 +5,17 @@ export type FindAllCompetitionJuniorLevelsParams = Pick<
   Paths.CompetitionJuniorLevelsControllerFindAll.QueryParameters,
   'level' | 'limit' | 'name' | 'page' | 'sortBy' | 'sortingOrder'
 >
+
+export type CreateCompetitionJuniorLevelDto =
+  Components.Schemas.CreateCompetitionJuniorLevelDto
+
+export type UpdateCompetitionJuniorLevelDto =
+  Components.Schemas.UpdateCompetitionJuniorLevelDto
+
+export type CompetitionJuniorLevelsFiltersDto = Pick<
+  FindAllCompetitionJuniorLevelsParams,
+  'level' | 'name'
+>
+
+export type CompetitionJuniorLevelsSortBy =
+  Paths.CompetitionJuniorLevelsControllerFindAll.Parameters.SortBy
