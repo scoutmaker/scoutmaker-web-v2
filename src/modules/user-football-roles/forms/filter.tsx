d7afc75d96e14d01d5eb562cec7a0549c057629d
dejwi <misti.krasula@gmail.com>
2022-08-25 11:59:05 +0200

    feat: add user football roles module

---
+++ src/modules/user-football-roles/forms/filter.tsx
@@ -0,0 +1,46 @@
+import { TextField } from '@mui/material'
+import { Field, Form, Formik } from 'formik'
+import { useTranslation } from 'next-i18next'
+
+import { Container } from '@/components/forms/container'
+import { FilterFormActions } from '@/components/forms/filter-form-actions'
+
+import {  UserFootballRolesFiltersDto } from '../types'
+
+interface IFormProps {
+  filters: UserFootballRolesFiltersDto
+  onFilter: (data: UserFootballRolesFiltersDto) => void
+  onClearFilters: () => void
+}
+
+export const UserFootballRolesFilterForm = ({
+  filters,
+  onFilter,
+  onClearFilters,
+}: IFormProps) => {
+  const { t } = useTranslation()
+
+  return (
+    <Formik
+      initialValues={filters}
+      onSubmit={data => onFilter(data)}
+      enableReinitialize
+    >
+      {() => (
+        <Form autoComplete="off">
+          <Container>
+            <Field
+              name="name"
+              as={TextField}
+              variant="outlined"
+              fullWidth
+              label={t('NAME')}
+              size="small"
+            />
+            <FilterFormActions handleClearFilter={onClearFilters} />
+          </Container>
+        </Form>
+      )}
+    </Formik>
+  )
+}
--- src/modules/user-football-roles/types.ts
+++ src/modules/user-football-roles/types.ts
@@ -1,1 +1,20 @@
 export type UserFootballRoleDto = Components.Schemas.UserFootballRoleDto
+
+export type CreateUserFootballRoleDto =
+  Components.Schemas.CreateUserFootballRoleDto
+
+export type UpdateUserFootballRoleDto =
+  Components.Schemas.UpdateUserFootballRoleDto
+
+export type FindAllUserFootballRolesDto = Pick<
+  Paths.UserFootballRolesControllerFindAll.QueryParameters,
+  'limit' | 'name' | 'page' | 'sortBy' | 'sortingOrder'
+>
+
+export type UserFootballRolesFiltersDto = Pick<
+  FindAllUserFootballRolesDto,
+  'name'
+>
+
+export type UserFootballRolesSortBy =
+  Paths.UserFootballRolesControllerFindAll.Parameters.SortBy
