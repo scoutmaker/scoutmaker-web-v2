546f7835029015d9d4d42e8f061ae89ac7978bf7
dejwi <misti.krasula@gmail.com>
2022-08-26 10:59:21 +0200

    feat: add competition groups module

---
+++ src/modules/competition-groups/froms/filter.tsx
@@ -0,0 +1,66 @@
+import { TextField } from '@mui/material'
+import { Field, Form, Formik } from 'formik'
+import { useTranslation } from 'next-i18next'
+
+import { Container } from '@/components/forms/container'
+import { FilterFormActions } from '@/components/forms/filter-form-actions'
+import { CompetitionsCombo } from '@/modules/competitions/combo'
+import { CompetitionBasicDataDto } from '@/modules/competitions/types'
+import { RegionsCombo } from '@/modules/regions/combo'
+import { RegionDto } from '@/modules/regions/types'
+
+import { CompetitionGroupsFiltersDto } from '../types'
+
+interface IFormProps {
+  filters: CompetitionGroupsFiltersDto
+  onFilter: (data: CompetitionGroupsFiltersDto) => void
+  onClearFilters: () => void
+  competitionsData: CompetitionBasicDataDto[]
+  regionsData: RegionDto[]
+}
+
+export const CompetitionGroupsFilterForm = ({
+  filters,
+  onFilter,
+  onClearFilters,
+  competitionsData,
+  regionsData
+}: IFormProps) => {
+  const { t } = useTranslation()
+
+  return (
+    <Formik
+      initialValues={filters}
+      onSubmit={data => onFilter(data)}
+      enableReinitialize
+    >
+      {() => (
+        <Form autoComplete="off">
+          <Container>
+            <Field
+              name="name"
+              as={TextField}
+              variant="outlined"
+              fullWidth
+              label={t('NAME')}
+              size="small"
+            />
+            <CompetitionsCombo
+              multiple
+              data={competitionsData}
+              name='competitionIds'
+              label={t('COMPETITIONS')}
+            />
+            <RegionsCombo
+              multiple
+              data={regionsData}
+              name='regionIds'
+              label={t('REGIONS')}
+            />
+            <FilterFormActions handleClearFilter={onClearFilters} />
+          </Container>
+        </Form>
+      )}
+    </Formik>
+  )
+}
