e281d332915fb10215a107b93d3d2f2c015e2ca0
dejwi <misti.krasula@gmail.com>
2022-09-12 15:42:10 +0200

    feat: add organization subscriptions pages

---
+++ src/pages/organization-subscriptions/[id].tsx
@@ -0,0 +1,43 @@
+import { useTranslation } from 'next-i18next'
+
+import { ErrorContent } from '@/components/error/error-content'
+import { PageHeading } from '@/components/page-heading/page-heading'
+import { OrganizationSubscriptionDetailsCard } from '@/modules/organization-subscriptions/details-card'
+import { OrganizationSubscriptionDto } from '@/modules/organization-subscriptions/types'
+import { getOrganizationSubscriptionById } from '@/services/api/methods/organization-subscriptions'
+import { ApiError } from '@/services/api/types'
+import { TSsrRole, withSessionSsrRole } from '@/utils/withSessionSsrRole'
+
+export const getServerSideProps = withSessionSsrRole<OrganizationSubscriptionDto>(
+  ['common', 'organization-subs'],
+  ['ADMIN'],
+  async (token, params) => {
+    try {
+      const data = await getOrganizationSubscriptionById(params?.id as string, token)
+      return { data }
+    } catch (error) {
+      return {
+        data: null,
+        error: error as ApiError,
+      }
+    }
+  },
+)
+
+const OrganizationSubscriptionPage = ({
+  data,
+  errorMessage,
+  errorStatus,
+}: TSsrRole<OrganizationSubscriptionDto>) => {
+  const { t } = useTranslation()
+
+  if (!data) return <ErrorContent message={errorMessage} status={errorStatus} />
+  return (
+    <>
+      <PageHeading title={t('ORGANIZATION_SUBSCRIPTION')} />
+      <OrganizationSubscriptionDetailsCard sub={data} />
+    </>
+  )
+}
+
+export default OrganizationSubscriptionPage
---
+++ src/pages/organization-subscriptions/create.tsx
@@ -0,0 +1,41 @@
+import { useTranslation } from 'next-i18next'
+
+import { ErrorContent } from '@/components/error/error-content'
+import { Loader } from '@/components/loader/loader'
+import { PageHeading } from '@/components/page-heading/page-heading'
+import { useCompetitionGroupsList } from '@/modules/competition-groups/hooks'
+import { useCompetitionsList } from '@/modules/competitions/hooks'
+import { CreateOrganizationSubscriptionForm } from '@/modules/organization-subscriptions/forms/create'
+import { useCreateOrganizationSubscription } from '@/modules/organization-subscriptions/hooks'
+import { useOrganizationsList } from '@/modules/organizations/hooks'
+import { TSsrRole, withSessionSsrRole } from '@/utils/withSessionSsrRole'
+
+export const getServerSideProps = withSessionSsrRole(['common', 'organization-subs'], ['ADMIN'])
+
+const CreateOrganizationSubscriptionPage = ({ errorMessage, errorStatus }: TSsrRole) => {
+  const { t } = useTranslation()
+
+  const { mutate: createOrgSub, isLoading: createLoading } = useCreateOrganizationSubscription()
+
+  const { data: organizations, isLoading: organizationsLoading } = useOrganizationsList()
+  const { data: compGroups, isLoading: compgroupsLoading } = useCompetitionGroupsList()
+  const { data: competitions, isLoading: competitionsLoading } = useCompetitionsList()
+
+  const isLoading = createLoading || organizationsLoading || compgroupsLoading || competitionsLoading
+
+  if (errorStatus) return <ErrorContent message={errorMessage} status={errorStatus} />
+  return (
+    <>
+      {isLoading && <Loader />}
+      <PageHeading title={t('organization-subs:CREATE_PAGE_TITLE')} />
+      <CreateOrganizationSubscriptionForm
+        onSubmit={createOrgSub}
+        organizationsData={organizations || []}
+        competitionGroupsData={compGroups || []}
+        competitionsData={competitions || []}
+      />
+    </>
+  )
+}
+
+export default CreateOrganizationSubscriptionPage
