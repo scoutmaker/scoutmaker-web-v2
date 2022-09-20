34d026358ac3533620142dfbea5348bd52c1bb6e
dejwi <misti.krasula@gmail.com>
2022-09-20 18:03:11 +0200

    feat: fix styling

--- src/components/forms/container.tsx
+++ src/components/forms/container.tsx
@@ -11,10 +11,6 @@ export const Container = styled('div')<IContainerProps>(
     margin: theme.spacing(0, 'auto', 2),
     gap: theme.spacing(2),
 
-    [theme.breakpoints.up('sm')]: {
-      width: '50%',
-    },
-
     width: fullwidth ? '100%' : 'auto',
   }),
 )
