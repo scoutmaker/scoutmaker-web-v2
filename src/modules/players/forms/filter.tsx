667392883539c5ae3217248e939836a160ae4291
dejwi <misti.krasula@gmail.com>
2022-09-29 13:47:25 +0200

    feat: change sizes to small

--- src/modules/players/forms/filter.tsx
+++ src/modules/players/forms/filter.tsx
@@ -95,30 +95,35 @@ export const PlayersFilterForm = ({
               name="countryIds"
               data={countriesData}
               label={t('COUNTRIES')}
+              size="small"
               multiple
             />
             <PlayersPositionCombo
               name="positionIds"
               data={positionsData}
               label={t('POSITIONS')}
+              size="small"
               multiple
             />
             <TeamsCombo
               data={teamsData}
               name="teamIds"
               label={t('TEAM')}
+              size="small"
               multiple
             />
             <CompetitionsCombo
               name="competitionIds"
               data={competitionsData}
               label={t('COMPETITIONS')}
+              size="small"
               multiple
             />
             <CompetitionGroupsCombo
               name="competitionGroupIds"
               data={competitionGroupsData}
               label={t('COMPETITION_GROUPS')}
+              size="small"
               multiple
             />
             <StyledCheckboxContainer>
