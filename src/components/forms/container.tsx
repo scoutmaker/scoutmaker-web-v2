7246793e21d88de52924f29198c6c270dbfdc231
dejwi <misti.krasula@gmail.com>
2022-09-30 15:20:17 +0200

    feat: form container styling

--- src/components/forms/container.tsx
+++ src/components/forms/container.tsx
@@ -6,13 +6,17 @@ interface IContainerProps {
 
 export const Container = styled('div')<IContainerProps>(
   ({ theme, fullwidth }) => ({
-    display: 'flex',
-    flexDirection: 'column',
+    display: 'grid',
     margin: theme.spacing(0, 'auto', 2),
     gap: theme.spacing(2),
 
-    [theme.breakpoints.up('sm')]: {
-      width: '50%',
+    // [theme.breakpoints.up('sm')]: {
+    //   width: '50%',
+    // },
+
+    gridTemplateColumns: '1fr 1fr 1fr',
+    [theme.breakpoints.down('lg')]: {
+      gridTemplateColumns: 'repeat(auto-fit, minmax(320px, 1fr))',
     },
 
     width: fullwidth ? '100%' : 'auto',
