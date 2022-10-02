a9622f21bff5985f184d2c01545d2a6e77b92cc8
jakubfiglak <jakub.figlak@netguru.pl>
2022-10-02 17:07:31 +0200

    feat: adjust rating range filter handler for notes module

--- src/modules/notes/utils.ts
+++ src/modules/notes/utils.ts
@@ -1,18 +1,12 @@
+import { NotesFilterFormData, NotesFiltersDto } from '@/modules/notes/types'
 import {
   RATING_RANGE_END_MAP,
   RATING_RANGE_START_MAP,
 } from '@/utils/rating-range-maps'
-import { Routes } from '@/utils/routes'
-
-import { ReportsFilterFormData, ReportsFiltersDto } from './types'
-
-export function getSingleReportRoute(id: string) {
-  return `${Routes.REPORTS}/${id}`
-}
 
 export function mapFilterFormDataToFiltersDto(
-  data: ReportsFilterFormData,
-): ReportsFiltersDto {
+  data: NotesFilterFormData,
+): NotesFiltersDto {
   const { ratingRange, ...rest } = data
 
   return {
---
+++ src/types/rating-range.ts
@@ -0,0 +1,6 @@
+export type RatingRange =
+  | 'ALL'
+  | 'NEGATIVE_SELECTION'
+  | 'NO_DECISION'
+  | 'TO_OBSERVE'
+  | 'POSITIVE_SELECTION'
