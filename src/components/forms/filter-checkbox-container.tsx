f736077b23380a0c5ef8d6c12b7a40918b095547
dejwi <misti.krasula@gmail.com>
2022-10-07 11:45:52 +0200

    feat: extract checkbox container component

---
+++ src/components/forms/filter-checkbox-container.tsx
@@ -0,0 +1,8 @@
+import { styled } from '@mui/material'
+
+export const FilterCheckboxContainer = styled('div')(({ theme }) => ({
+  display: 'flex',
+  justifyContent: 'center',
+  marginTop: theme.spacing(-1),
+  marginBottom: theme.spacing(1),
+}))
