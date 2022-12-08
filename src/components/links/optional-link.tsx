28844353208e5a133dc2f8b8372d0f39d64aaac5
dejwi <misti.krasula@gmail.com>
2022-12-08 15:31:55 +0100

    remove passhref

--- src/components/links/optional-link.tsx
+++ src/components/links/optional-link.tsx
@@ -3,17 +3,10 @@ import Link from 'next/link'
 export const OptionalLinkWrapper = ({
   href,
   children,
-  passhref,
 }: {
   href?: string
   children: JSX.Element
-  passhref?: boolean
 }) => {
-  if (href)
-    return (
-      <Link href={href} passHref={passhref}>
-        {children}
-      </Link>
-    )
+  if (href) return <Link href={href}>{children}</Link>
   return <div>{children}</div>
 }
