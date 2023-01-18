b2be794dd0ec8a877858c0259222a0ff26a22e0e
dejwi <misti.krasula@gmail.com>
2023-01-18 15:31:52 +0100

    remove ids in detailed notes&reports

--- src/components/breadcrumbs/utils.ts
+++ src/components/breadcrumbs/utils.ts
@@ -9,7 +9,12 @@ export function generateBreadcrumbs(router: NextRouter) {
 
   const crumblist = asPathNestedRoutes.map((path, idx) => {
     const href = `/${asPathNestedRoutes.slice(0, idx + 1).join('/')}`
-    const title = path
+
+    let title = path
+    if (href.includes('/reports/') || href.includes('/notes/')) {
+      if (title.includes('-')) title = title.slice(title.indexOf('-') + 1)
+    }
+
     return { href, title }
   })
 
